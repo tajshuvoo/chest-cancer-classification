231bb5686cdc3d4ba6f3b0253e61f34512da6f23
tajshuvoo <tajshuvo75@gmail.com>
2026-02-04 22:28:03 +0600

    training module added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chest_cancer_classification import logger
 from chest_cancer_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chest_cancer_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chest_cancer_classification.pipeline.stage_03_model_train import ModelTrainingPipeline
 
 STAGE_NAME ="Data Ingestion stage"
 
@@ -21,6 +22,18 @@ try:
     logger.info(f"**************************************")
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"**************************************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 except Exception as e:
